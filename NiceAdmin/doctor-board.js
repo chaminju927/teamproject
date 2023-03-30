db326a41f5d4fcffcec64d1da21b112831bcc5b7
whtndgus <whtndgus12@gmail.com>
2023-03-30 19:44:12 +0900

    .

--- NiceAdmin/doctor-board.js
+++ NiceAdmin/doctor-board.js
@@ -1,15 +1,8 @@
-class BoardLists extends React.Component {
+class Board extends React.Component {
   constructor(props) {
     super(props);
     props = props.props;
     this.state = {
-      answer: props.answer,
-      no: props.no,
-      title: props.title,
-      warn: props.warn,
-      writer: props.writer,
-      date: props.date,
-      warnLevel: props.warnLevel
     }
   }
   render() {
@@ -18,7 +11,28 @@ class BoardLists extends React.Component {
   }
 }
 
-if(window.localStorage.getItem("boardNo") != null){
+function BoardImg(props) {
+  return (<img className="d-block w-100" src={props.url} alt="..." ></img>)
+}
+
+class BoardImgDiv extends React.Component {
+  constructor(props) {
+    super(props);
+    props = props.props;
+    this.state = {
+      img: props
+    }
+  }
+  render() {
+    return(
+      <div className="carousel-item active">
+        <BoardImg url={this.state.img} />
+      </div>
+    )
+  }
+}
+
+if (window.localStorage.getItem("boardNo") != null) {
   let str = window.localStorage.getItem("boardNo");
   console.log(str)
   fetch("http://192.168.0.7:8080/boardNo", {
@@ -30,18 +44,41 @@ if(window.localStorage.getItem("boardNo") != null){
       no: str
     })
   })
-  .then(response => response.json())
-  .then(data => {
-    data = data.data;
-    console.log(data);
-    $(".board-title").html("제목 : " + data.title);
-    $(".board-pain").html("증상 : " + data.pain);
-    $(".board-another").html("그 외 전달사항 : " + (data.another.split(",")[5] != "null" && data.another.split(",")[5].length > 0 ? data.another.split(",")[5] : "-"));
-    $(".board-name").val(data.another.split(",")[0] != "null" && data.another.split(",")[0].length > 0 ? data.another.split(",")[0] : "-");
-    $(".board-gender").val(data.another.split(",")[2] != "null" && data.another.split(",")[2].length > 0 ? data.another.split(",")[2] : "-");
-    $(".board-age").val(data.another.split(",")[1] != "null" && data.another.split(",")[1].length > 0 ? data.another.split(",")[1] : "-");
-    $(".board-addr").val(data.another.split(",")[4] != "null" && data.another.split(",")[4].length > 0 ? data.another.split(",")[4] : "-");
-    $(".board-tel").val(data.another.split(",")[3] != "null" && data.another.split(",")[3].length > 0 ? data.another.split(",")[3] : "-");
-  })
-}
+    .then(response => response.json())
+    .then(data => {
+      data = data.data;
+      $(".board-title").html("제목 : " + data.title);
+      $(".board-pain").html("증상 : " + data.pain);
+      $(".board-another").html("그 외 전달사항 : " + (data.another.split(",")[5] != "null" && data.another.split(",")[5].length > 0 ? data.another.split(",")[5] : "-"));
+      $(".board-name").val(data.another.split(",")[0] != "null" && data.another.split(",")[0].length > 0 ? data.another.split(",")[0] : "-");
+      $(".board-gender").val(data.another.split(",")[2] != "null" && data.another.split(",")[2].length > 0 ? data.another.split(",")[2] : "-");
+      $(".board-age").val(data.another.split(",")[1] != "null" && data.another.split(",")[1].length > 0 ? data.another.split(",")[1] : "-");
+      $(".board-addr").val(data.another.split(",")[4] != "null" && data.another.split(",")[4].length > 0 ? data.another.split(",")[4] : "-");
+      $(".board-tel").val(data.another.split(",")[3] != "null" && data.another.split(",")[3].length > 0 ? data.another.split(",")[3] : "-");
+    })
 
+  fetch("http://192.168.0.7:8080/findAllBoardImg", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ // 스프링에 전달할 값
+      bno: str
+    })
+  }).then(response => response.json())
+    .then(data => {
+      if (data.status == "success") {
+        data = data.data;
+        let imgs = [];
+        data.forEach(value => {
+          let url = value.url;
+          imgs.push(<BoardImgDiv props={url} />)
+        });
+        console.log(imgs)
+        ReactDOM.createRoot(document.getElementById('imgs')).render(
+          imgs
+        );
+      }
+
+    })
+}
