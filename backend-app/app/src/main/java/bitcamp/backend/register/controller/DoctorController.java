955b14433ec6bcb4ce1c01de2c74481e44175dc7
omelase <11chan11@naver.com>
2023-03-31 13:46:39 +0900

    hospitalNo 타입 int -> String 변경

--- backend-app/app/src/main/java/bitcamp/backend/register/controller/DoctorController.java
+++ backend-app/app/src/main/java/bitcamp/backend/register/controller/DoctorController.java
@@ -3,6 +3,7 @@ package bitcamp.backend.register.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import bitcamp.util.RestStatus;
 
 @RestController
 @RequestMapping("/doctors")
+@CrossOrigin("*")
 public class DoctorController {
 
   Logger log = LogManager.getLogger(getClass());
