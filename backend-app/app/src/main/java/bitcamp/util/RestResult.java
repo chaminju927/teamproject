8450611ce23ad634baf22cd5dfb4f3c4ee164a3f
omelase <11chan11@naver.com>
2023-04-03 12:11:23 +0900

    RestResult 코드 추가

--- backend-app/app/src/main/java/bitcamp/util/RestResult.java
+++ backend-app/app/src/main/java/bitcamp/util/RestResult.java
@@ -4,6 +4,7 @@ public class RestResult {
   String status;
   String errorCode;
   Object data;
+  String message;
 
   public String getStatus() {
     return status;
@@ -24,6 +25,13 @@ public class RestResult {
   }
   public RestResult setData(Object data) {
     this.data = data;
+    return this;
+  }
+  public String getMessage() {
+    return message;
+  }
+  public RestResult setMessage(String message) {
+    this.message = message;
     return this;
   }
 }
