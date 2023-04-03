aea456f4f94b29d6f4a025bc45e85a383cc73034
chaminju927 <minjoo92718@gmail.com>
2023-04-03 18:15:25 +0900

    .

--- backend-app/app/src/main/java/bitcamp/backend/community/dao/CommunityDao.java
+++ backend-app/app/src/main/java/bitcamp/backend/community/dao/CommunityDao.java
@@ -1,5 +1,16 @@
 package bitcamp.backend.community.dao;
 
-public class CommunityDao {
+import java.util.List;
+import bitcamp.backend.community.vo.Community;
+
+public interface CommunityDao {
+
+  void insert(Community community);
+  List<Community> findAll();
+  Community findByNo(int no);
+  void increaseViewCount(int no);
+  int update(Community c);
+  int delete(int no);
+
 
 }
