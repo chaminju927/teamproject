a38e32167199beaafc7674dd0113f31a7d57300d
chaminju927 <minjoo92718@gmail.com>
2023-04-04 13:54:04 +0900

    .

---
+++ backend-app/app/src/main/java/bitcamp/backend/community/vo/CommImg.java
@@ -0,0 +1,14 @@
+package bitcamp.backend.community.vo;
+
+import lombok.Data;
+
+@Data
+public class CommImg {
+
+  private int no;
+  private int com_no;
+  private String url;
+  private String name;
+  private String MIMETYPE;
+
+}
