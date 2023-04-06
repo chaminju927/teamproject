31f5b075ed618f3949171f8d6513deca53c336fc
omelase <11chan11@naver.com>
2023-04-06 20:25:12 +0900

    라이센스vo수정

--- backend-app/app/src/main/java/bitcamp/backend/register/vo/License.java
+++ backend-app/app/src/main/java/bitcamp/backend/register/vo/License.java
@@ -4,7 +4,14 @@ import lombok.Data;
 
 @Data
 public class License {
+  private Doctor doctor;
+  public int getNo() {
+    return doctor.getNo();
+  }
   private LicenseCategory category;
+  public int getLicenseNo() {
+    return category.getLicenseNo();
+  }
   private String licensePhoto;
   private String phoFilename;
   private String phoType;
