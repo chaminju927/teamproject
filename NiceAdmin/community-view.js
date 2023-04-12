51977afffdb9e76a5d49c557f33bfed78d69b917
chaminju927 <minjoo92718@gmail.com>
2023-04-12 11:42:58 +0900

    .

--- NiceAdmin/community-view.js
+++ NiceAdmin/community-view.js
@@ -5,62 +5,56 @@ document.querySelector('#former-btn').onclick = (e) => {
   location.href='community-list.html';
 };
 
-fetch(`http://localhost:8080/community/${no}`)
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
 
+Promise.all([
+  fetch(`http://localhost:8080/community/${no}`),
+  fetch(`http://localhost:8080/recomment/${no}`)
+])
+.then((responses) => Promise.all(responses.map((response) => response.json())))
+.then((data) => {
   console.log(data);
-    if (data.status == 'failure') {
-      // alert('서버 요청 오류!');
-      console.log(data);
-      return;
-    }
-  document.querySelector('#title').value = data.data.title;
-  document.querySelector('#category').value = data.data.category;
-  document.querySelector('#doctorName').value = data.data.doctorName;
-  document.querySelector('#createdDate').value = data.data.createdDate;
-  document.querySelector('#content').value = data.data.content;
-  if(data.photo.length >0 ) {
-    $('#comImg')[0].src = data.photo[0].imgUrl;
+  const communityData = data[0];
+  const recommentData = data[1];
+
+  //console.log(communityData);
+  if (communityData.status == 'failure') {
+    // alert('서버 요청 오류!');
+   // console.log(communityData);
+    return;
+  }
+  document.querySelector('#title').value = communityData.data.title;
+  document.querySelector('#category').value = communityData.data.category;
+  document.querySelector('#doctorName').value = communityData.data.doctorName;
+  document.querySelector('#createdDate').value = communityData.data.createdDate;
+  document.querySelector('#content').value = communityData.data.content;
+  if(communityData.photo.length >0 ) {
+    $('#comImg')[0].src = communityData.photo[0].imgUrl;
   } else {
     $("#comImg").attr('src', '');
     document.querySelector('#btn-img-delete').style.display = 'none';
   }
-   console.log(data.photo[0].imgUrl)
+  // console.log(communityData.photo[0].imgUrl)
+  // 두번째 fetch 요청 후
+  var tbody = document.querySelector('#recomment-list');
+
+  console.log(recommentData);
+
+  var html = '';
+  for (var row of recommentData.data) {
+    html += `<tr>
+        <td>${row.recNo}</td>
+        <td><p>${row.recContent}</p></td>
+        <td>${row.docName}</td> 
+        <td>${row.createdDate}</td>
+        </tr>\n`;
+  }
+  tbody.innerHTML = html;
 })
 .catch((err) => {
   //alert('서버 요청 오류!');
   console.log(err);
 });
 
-
-//댓글 리스트
-tbody = document.querySelector('#recomment-list');
-
-fetch('http://localhost:8080/recomment/list')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    var html = '';
-    for (var row of data.data) {
-      html += `<tr>
-          <td>${row.recNo}</td>
-          <td><p>${row.recContent}</p></td>
-          <td>${row.docName}</td> 
-          <td>${row.createdDate}</td>
-          </tr>\n`;
-    }
-    console.log(data);
-    tbody.innerHTML = html;
-  })
-  .catch((err) => {
-    alert('서버 요청 오류!');
-    console.log(err);
-  });
 
 //댓글 입력
 document.querySelector('#rec-save-btn').onclick = (e) => {
@@ -72,13 +66,15 @@ document.querySelector('#rec-save-btn').onclick = (e) => {
     },
     body: JSON.stringify({
       docNo: 1,
-      comNo: Number(no),
+      comNo: no,
       recContent: document.querySelector('.recomment').value
     }),
   })
     .then((response) => response.json())
     .then((data) => {
       console.log("성공:", data);
+      location.reload();
+
     })
     .catch((error) => {
       console.error("실패:", error);
@@ -158,23 +154,3 @@ document.querySelector('#update-btn').onclick = (e) => {
     });
     };
   
-
-    // function deleteImg(no) {
-    //     fetch(`http://localhost:8080/communityImg/${no}`,{
-    //     method: 'DELETE',
-    //   })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log("성공:", data);
-    //     document.querySelector('#btn-img-delete').style.display = 'none';
-    
-    //     if (data.status == 'failure') {
-    //       alert('삭제 실패!\n' + data.data);
-    //       return;
-    //     } 
-    //   })
-    //   .catch((error) => {
-    //     console.log("실패:", error);
-    //   });
-    //   };
-    
