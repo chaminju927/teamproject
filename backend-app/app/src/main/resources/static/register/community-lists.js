1d65e5e8400fc8548536176e39a8987ad8588992
whtndgus <whtndgus12@gmail.com>
2023-04-25 14:31:11 +0900

    환자 회원 네이버 로그인시 등록 정보 입력 페이지 연결 및 정보 들록된 계정은 로그인 가능, 의사 커뮤니티 페이지 네이버 검색 기능 페이지 카운트 기능 추가

--- backend-app/app/src/main/resources/static/register/community-lists.js
+++ backend-app/app/src/main/resources/static/register/community-lists.js
@@ -10,7 +10,7 @@ function categoryName(category) {
   switch (category) {
     case 1:
       return '자유게시판';
-    case 2:j
+    case 2:
       return '의학뉴스';
     case 3:
       return '질문게시판';
@@ -34,9 +34,9 @@ fetch(`http://localhost:8080/auth/user`, {
   })
   .then((user) => {
     console.log(user.hosName !== undefined)
-    if(user.hosName !== undefined) {
+    if (user.hosName !== undefined) {
       myno = user.no
-    }else {
+    } else {
       console.log(user.hosName)
       location.href = "../auth/doctors-login.html"
     }
@@ -84,75 +84,99 @@ fetch(`http://localhost:8080/auth/user`, {
       .catch((err) => {
         console.log(err);
       });
-    })
+  })
 
 // 네이버 검색 API
-    
+let currentPage = 1;
+let list = [];
 document.querySelector('.naver-btn').onclick = (e) => {
   var searchNaver = document.querySelector('.naver-search').value;
   document.querySelector('#when-searched').style.display = 'block';
+
+  async function getData() {
+    const response = await fetch(`http://localhost:8080/community/search?query=${searchNaver}`);
+    const data = await response.json();
+    return data;
+  }
+  getData().then(data => {
+    // console.log(data);
+    if (data.status == "success") {
+      //console.log("성공:", JSON.parse(data.data));
+      list = JSON.parse(data.data).items;
+      console.log(list);
+      searchreflesh(currentPage)
+    }
     
-    async function getData() {
-      const response = await fetch(`http://localhost:8080/community/search?query=${searchNaver}`);
-      const data = await response.json();
-      return data;
-   }
-    getData().then(data => {
-   // console.log(data);
-      if (data.status == "success") {
-        //console.log("성공:", JSON.parse(data.data));
-        let list = JSON.parse(data.data).items;
-        console.log(list);
-        currentPage = 1;
-     
-    /*  document.querySelector('#page-link-1').onclick = (e) => {
-        currentPage = 2;  
-      }
-      document.querySelector('#page-link-2').onclick = (e) => {
-        currentPage = 3;
-      }*/
-      switch(currentPage) {
-        case 1 :
-           for (let i = 0; i < 3; i++) { // 현재 페이지에서 출력할 데이터만 출력
-            document.querySelector(`#search-title-${i}`).innerHTML = list[i].title;
-            document.querySelector(`#search-desc-${i}`).innerHTML = list[i].description;
-            document.querySelector(`#search-blogger-${i}`).innerHTML = list[i].bloggername;
-            document.querySelector(`#search-date-${i}`).innerHTML = list[i].postdate;
-            document.querySelector(`#search-${i}`).onclick = createOnClickFunction(list[i].link);
-          }
-          break;
-        case 2 :
-           for (let i = 3; i < 6; i++) { // 현재 페이지에서 출력할 데이터만 출력
-            document.querySelector(`#search-title-${i}`).innerHTML = list[i].title;
-            document.querySelector(`#search-desc-${i}`).innerHTML = list[i].description;
-            document.querySelector(`#search-blogger-${i}`).innerHTML = list[i].bloggername;
-            document.querySelector(`#search-date-${i}`).innerHTML = list[i].postdate;
-            document.querySelector(`#search-${i}`).onclick = createOnClickFunction(list[i].link);
-          }   
-          break;
-        case 3 :
-           for (let i = 6; i < 8; i++) { // 현재 페이지에서 출력할 데이터만 출력
-            document.querySelector(`#search-title-${i}`).innerHTML = list[i].title;
-            document.querySelector(`#search-desc-${i}`).innerHTML = list[i].description;
-            document.querySelector(`#search-blogger-${i}`).innerHTML = list[i].bloggername;
-            document.querySelector(`#search-date-${i}`).innerHTML = list[i].postdate;
-            document.querySelector(`#search-${i}`).onclick = createOnClickFunction(list[i].link);
-          }
-          break;
-      default : return;
-      }
-     
-      function createOnClickFunction(link) {
-        return (e) => {
-          location.href = link;
-        }
-      }
-        
-     }      
-    }).
+
+    document.querySelector('.page-link-0').onclick = (e) => {
+      currentPage = 1;
+      searchreflesh(1)
+    }
+    document.querySelector('.page-link-1').onclick = (e) => {
+      currentPage = 2;
+      searchreflesh(2)
+    }
+    document.querySelector('.page-link-2').onclick = (e) => {
+      currentPage = 3;
+      searchreflesh(3)
+    }
+  }).
     catch(err => {
       //alert('서버 요청 오류!');
       console.log(err);
     });
 }
 
+function searchreflesh(currentPage) {
+  switch (currentPage) {
+    case 1:
+      for (let i = 0; i < 3; i++) { // 현재 페이지에서 출력할 데이터만 출력
+        $(`#search-${i}`).css("display","");
+        document.querySelector(`#search-title-${i}`).innerHTML = list[i].title;
+        document.querySelector(`#search-desc-${i}`).innerHTML = list[i].description;
+        document.querySelector(`#search-blogger-${i}`).innerHTML = list[i].bloggername;
+        document.querySelector(`#search-date-${i}`).innerHTML = list[i].postdate;
+        document.querySelector(`#search-${i}`).onclick = createOnClickFunction(list[i].link);
+        for (let a = 3; a < 9; a++) {
+          $(`#search-${a}`).css("display","none")
+        }
+      }
+      break;
+    case 2:
+      for (let i = 3; i < 6; i++) { // 현재 페이지에서 출력할 데이터만 출력
+        $(`#search-${i}`).css("display","");
+        document.querySelector(`#search-title-${i}`).innerHTML = list[i].title;
+        document.querySelector(`#search-desc-${i}`).innerHTML = list[i].description;
+        document.querySelector(`#search-blogger-${i}`).innerHTML = list[i].bloggername;
+        document.querySelector(`#search-date-${i}`).innerHTML = list[i].postdate;
+        document.querySelector(`#search-${i}`).onclick = createOnClickFunction(list[i].link);
+        for (let a = 0; a < 3; a++) {
+          $(`#search-${a}`).css("display","none")
+        }
+        for (let a = 6; a < 9; a++) {
+          $(`#search-${a}`).css("display","none")
+        }
+      }
+      break;
+    case 3:
+      for (let i = 6; i < 9; i++) { // 현재 페이지에서 출력할 데이터만 출력
+        $(`#search-${i}`).css("display","");
+        document.querySelector(`#search-title-${i}`).innerHTML = list[i].title;
+        document.querySelector(`#search-desc-${i}`).innerHTML = list[i].description;
+        document.querySelector(`#search-blogger-${i}`).innerHTML = list[i].bloggername;
+        document.querySelector(`#search-date-${i}`).innerHTML = list[i].postdate;
+        document.querySelector(`#search-${i}`).onclick = createOnClickFunction(list[i].link);
+        for (let a = 0; a < 6; a++) {
+          $(`#search-${a}`).css("display","none")
+        }
+      }
+      break;
+    default: return;
+  }
+}
+
+function createOnClickFunction(link) {
+  return (e) => {
+    location.href = link;
+  }
+}
